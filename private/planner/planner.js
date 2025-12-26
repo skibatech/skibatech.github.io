729c109f0852b215b0361b68fb84f4449918247c
Mark Skiba <maskiba@skibatech.com>
2025-12-26 01:13:33 -0500

    Move quote above date; center date; make pencil button transparent; bump to 1.5.0

--- private/planner/planner.js
+++ private/planner/planner.js
@@ -1,5 +1,5 @@
 // Application Version - Update this with each change
-const APP_VERSION = '1.4.98'; // Change Edit button to pencil icon; Done to checkmark
+const APP_VERSION = '1.5.0'; // Pencil button matches background; quote above date; centered date
 
 // Compact set of one-line motivational quotes (max ~60 chars)
 const MOTIVATIONAL_QUOTES = [
