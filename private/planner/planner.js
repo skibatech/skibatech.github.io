0a510c0b11e6ca43826627b1c291061e60417c25
Mark Skiba <maskiba@skibatech.com>
2025-12-26 01:19:17 -0500

    Add divider under quote row; bump to 1.5.2

--- private/planner/planner.js
+++ private/planner/planner.js
@@ -1,5 +1,5 @@
 // Application Version - Update this with each change
-const APP_VERSION = '1.5.1'; // Date inline single-line; minor layout tweak
+const APP_VERSION = '1.5.2'; // Add divider under quote row
 
 // Compact set of one-line motivational quotes (max ~60 chars)
 const MOTIVATIONAL_QUOTES = [
